ce8ddc64bebe5cf69e087b4de9bc8b9db4669876
Simon <simonpost98@gmail.com>
2019-07-10 00:02:55 +0200

    Main UI done
    App searches for scanners (untested) and should display a dialog with more information if a scanner is indeed discovered.
    
    Removed splash screen due to this being a fast-launching utility application. Looks pretty cool.

--- Scanner/MainPage.xaml.cs
+++ Scanner/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Scanners;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -17,14 +19,70 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Scanner
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private Windows.UI.Xaml.Controls.StackPanel[] radioButtonStackPanels;
+        private DeviceWatcher scannerWatcher;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.radioButtonStackPanels = new Windows.UI.Xaml.Controls.StackPanel[] { StackPanelSource, StackPanelColor };
+            refreshScannerList();
+        }
+
+        public void refreshScannerList()
+        {
+            // Create a Device Watcher class for type Image Scanner for enumerating scanners
+            scannerWatcher = DeviceInformation.CreateWatcher(DeviceClass.ImageScanner);
+
+            scannerWatcher.Added += OnScannerAdded;
+            scannerWatcher.Removed += OnScannerRemoved;
+            scannerWatcher.EnumerationCompleted += OnScannerEnumerationComplete;
+        }
+
+        private async void OnScannerAdded(DeviceWatcher sender, DeviceInformation deviceInfo)
+        {
+            await
+            this.Dispatcher.RunAsync(
+                  Windows.UI.Core.CoreDispatcherPriority.Normal,
+                  async () => 
+                  {
+                      ContentDialog dialog = new ContentDialog
+                      {
+                          Title = "New device added",
+                          Content = "Id=" + deviceInfo.Id + " | Name=" + deviceInfo.Name + " | Kind=" + deviceInfo.Kind,
+                          CloseButtonText = "Ok"
+                      };
+
+
+                      ContentDialogResult result = await dialog.ShowAsync();
+
+                    // search the device list for a device with a matching device id
+                    /*ScannerDataItem match = FindInList(deviceInfo.Id);
+
+                    // If we found a match then mark it as verified and return
+                    if (match != null)
+                        {
+                            match.Matched = true;
+                            return;
+                        }
+
+                    // Add the new element to the end of the list of devices
+                    AppendToList(deviceInfo);*/
+                  }
+            );
+        }
+
+        private async void OnScannerRemoved(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+
+        }
+
+        private async void OnScannerEnumerationComplete(DeviceWatcher sender, Object theObject)
+        {
+
         }
     }
 }
