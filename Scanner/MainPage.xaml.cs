eaa2f879ad42e76bb4c5d3abddd6a46036f01191
Simon <simonpost98@gmail.com>
2019-07-10 17:08:39 +0200

    Can now find scanners and add them to the ComboBox
    Implemented button for opening the "Pictures\Scans" folder.

--- Scanner/MainPage.xaml.cs
+++ Scanner/MainPage.xaml.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Scanners;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,8 +20,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Scanner
 {
@@ -25,6 +29,7 @@ namespace Scanner
         private Windows.UI.Xaml.Controls.StackPanel[] radioButtonStackPanels;
         private DeviceWatcher scannerWatcher;
         private double ColumnLeftDefaultMaxWidth;
+        private ObservableCollection<string> scannerList = new ObservableCollection<string>();
 
         public MainPage()
         {
@@ -36,55 +41,45 @@ namespace Scanner
 
         public void refreshScannerList()
         {
+            Debug.WriteLine("refreshing scanner list");
+            ProgressBarRefresh.IsEnabled = true;
             // Create a Device Watcher class for type Image Scanner for enumerating scanners
             scannerWatcher = DeviceInformation.CreateWatcher(DeviceClass.ImageScanner);
 
             scannerWatcher.Added += OnScannerAdded;
             scannerWatcher.Removed += OnScannerRemoved;
             scannerWatcher.EnumerationCompleted += OnScannerEnumerationComplete;
+
+            scannerWatcher.Start();
         }
 
         private async void OnScannerAdded(DeviceWatcher sender, DeviceInformation deviceInfo)
         {
-            await
-            this.Dispatcher.RunAsync(
-                  Windows.UI.Core.CoreDispatcherPriority.Normal,
-                  async () => 
-                  {
-                      ContentDialog dialog = new ContentDialog
-                      {
-                          Title = "New device added",
-                          Content = "Id=" + deviceInfo.Id + " | Name=" + deviceInfo.Name + " | Kind=" + deviceInfo.Kind,
-                          CloseButtonText = "Ok"
-                      };
+            Debug.WriteLine("OnScannerAdded() with " + deviceInfo.ToString());
 
-
-                      ContentDialogResult result = await dialog.ShowAsync();
-
-                    // search the device list for a device with a matching device id
-                    /*ScannerDataItem match = FindInList(deviceInfo.Id);
-
-                    // If we found a match then mark it as verified and return
-                    if (match != null)
-                        {
-                            match.Matched = true;
-                            return;
-                        }
-
-                    // Add the new element to the end of the list of devices
-                    AppendToList(deviceInfo);*/
-                  }
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+                {
+                    scannerList.Add(deviceInfo.Name);
+                }
             );
         }
 
         private async void OnScannerRemoved(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
         {
-
+            Debug.WriteLine("OnScannerRemoved() with " + deviceInfoUpdate.ToString());
         }
 
         private async void OnScannerEnumerationComplete(DeviceWatcher sender, Object theObject)
         {
+            Debug.WriteLine("OnScannerEnumerationComplete()");
 
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+                {
+                    ProgressBarRefresh.Visibility = Visibility.Collapsed;
+                }
+            );
         }
 
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -111,6 +106,17 @@ namespace Scanner
             {
                 StackPanelTextRight.Opacity = 1.0;
             }
+        }
+
+        private void Hyperlink_Click(Windows.UI.Xaml.Documents.Hyperlink sender, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+            refreshScannerList();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await KnownFolders.PicturesLibrary.GetFolderAsync("Scans");
+            await Launcher.LaunchFolderAsync(folder);
         }
     }
 }
