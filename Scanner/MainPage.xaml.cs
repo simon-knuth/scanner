simon-knuth/scanner

commit a4f26129bf25b863cecbed16a7a06660e97a5057

Catch more exceptions and improve tooltips
refreshLeftPanel() should now cause less exceptions.

The new rating popup won't crash the app anymore if something goes wrong.

The tooltips are now more descriptive.

Scanner/MainPage.xaml.cs
@@ -914,7 +914,7 @@ namespace Scanner
 
             // update UI
             Page_SizeChanged(null, null);
-            refreshLeftPanel();
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
             ButtonDevices.IsEnabled = true;
         }
 
@@ -940,7 +940,7 @@ namespace Scanner
         /// <summary>
         ///     Reverts UI and variable changes that were made by commencing a scan.
         /// </summary>
-        private void ScanCanceled()
+        private async void ScanCanceled()
         {
             ShowPrimaryMenuConfig(PrimaryMenuConfig.hidden);
             ShowSecondaryMenuConfig(SecondaryMenuConfig.hidden);
@@ -953,7 +953,7 @@ namespace Scanner
             ButtonDevices.IsEnabled = true;
 
             Page_SizeChanged(null, null);
-            refreshLeftPanel();
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
         }
 
 
@@ -967,14 +967,16 @@ namespace Scanner
         ///     Is called if another source mode was selected. Hides/shows available options in the left panel and updates the available file formats. 
         ///     The first available color mode and format are automatically selected.
         /// </summary>
-        private void RadioButtonSourceChanged(object sender, RoutedEventArgs e)
+        private async void RadioButtonSourceChanged(object sender, RoutedEventArgs e)
         {
+            IAsyncAction leftPanelRefresh;
+
             if (RadioButtonSourceAutomatic.IsChecked == true)
             {
                 StackPanelColor.Visibility = Visibility.Collapsed;
                 StackPanelResolution.Visibility = Visibility.Collapsed;
 
-                refreshLeftPanel();
+                leftPanelRefresh = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
 
                 RadioButtonColorModeColor.IsEnabled = false;
                 RadioButtonColorModeGrayscale.IsEnabled = false;
@@ -984,18 +986,22 @@ namespace Scanner
                 RadioButtonColorModeGrayscale.IsChecked = false;
                 RadioButtonColorModeMonochrome.IsChecked = false;
 
+                await leftPanelRefresh;
+
                 // detect available file formats and update UI accordingly
                 GetSupportedFormats(selectedScanner.AutoConfiguration, formats, selectedScanner, ComboBoxFormat);
                 ComboBoxFormat.IsEnabled = true;
             }
             else if (RadioButtonSourceFlatbed.IsChecked == true)
             {
-                refreshLeftPanel();
+                leftPanelRefresh = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
 
                 // detect available color modes and update UI accordingly
                 RadioButtonColorModeColor.IsEnabled = selectedScanner.FlatbedConfiguration.IsColorModeSupported(ImageScannerColorMode.Color);
                 RadioButtonColorModeGrayscale.IsEnabled = selectedScanner.FlatbedConfiguration.IsColorModeSupported(ImageScannerColorMode.Grayscale);
                 RadioButtonColorModeMonochrome.IsEnabled = selectedScanner.FlatbedConfiguration.IsColorModeSupported(ImageScannerColorMode.Monochrome);
+
+                await leftPanelRefresh;
 
                 if (RadioButtonColorModeColor.IsEnabled) RadioButtonColorModeColor.IsChecked = true;
                 else if (RadioButtonColorModeGrayscale.IsEnabled) RadioButtonColorModeGrayscale.IsChecked = true;
@@ -1015,12 +1021,14 @@ namespace Scanner
             }
             else if (RadioButtonSourceFeeder.IsChecked == true)
             {
-                refreshLeftPanel();
+                leftPanelRefresh = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
 
                 // detect available color modes and update UI accordingly
                 RadioButtonColorModeColor.IsEnabled = selectedScanner.FeederConfiguration.IsColorModeSupported(ImageScannerColorMode.Color);
                 RadioButtonColorModeGrayscale.IsEnabled = selectedScanner.FeederConfiguration.IsColorModeSupported(ImageScannerColorMode.Grayscale);
                 RadioButtonColorModeMonochrome.IsEnabled = selectedScanner.FeederConfiguration.IsColorModeSupported(ImageScannerColorMode.Monochrome);
+
+                await leftPanelRefresh;
 
                 if (RadioButtonColorModeColor.IsEnabled) RadioButtonColorModeColor.IsChecked = true;
                 else if (RadioButtonColorModeGrayscale.IsEnabled) RadioButtonColorModeGrayscale.IsChecked = true;
@@ -1358,10 +1366,10 @@ namespace Scanner
         ///     The event listener for when a new scanner has been selected from the <see cref="ComboBoxScanners"/>.
         ///     Disabled the <see cref="ComboBox"/> to let <see cref="refreshLeftPanel"/> deal with this safely.
         /// </summary>
-        private void ComboBoxScanners_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void ComboBoxScanners_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ComboBoxScanners.IsEnabled = false;
-            refreshLeftPanel();
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
         }
 
 
@@ -1395,13 +1403,13 @@ namespace Scanner
         /// <summary>
         ///     The event listener for when a key is lifted on the MainPage. Used to process shortcuts.
         /// </summary>
-        private void MainPage_KeyUp(CoreWindow sender, KeyEventArgs args)
+        private async void MainPage_KeyUp(CoreWindow sender, KeyEventArgs args)
         {
             if ((!IsCtrlKeyPressed() || args.VirtualKey == VirtualKey.D) && debugShortcutActive)
             {
                 debugShortcutActive = false;
                 ButtonScan.IsEnabled = false;
-                refreshLeftPanel();
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, () => refreshLeftPanel());
             }
         }
 

Scanner/Strings/de/Resources.resw
@@ -122,7 +122,7 @@
     <comment>The text inside the button that allows the user to set an aspect ratio while cropping the result.</comment>
   </data>
   <data name="AppBarButtonAspectRatio.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Seitenverhältnis</value>
+    <value>Seitenverhältnis wählen</value>
     <comment>The tooltip text that appears while hovering the aspect ratio button.</comment>
   </data>
   <data name="AppBarButtonCopy.Label" xml:space="preserve">
@@ -130,7 +130,7 @@
     <comment>The text inside the button that allows the user to copy the result.</comment>
   </data>
   <data name="AppBarButtonCopy.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Kopieren (Strg+C)</value>
+    <value>In Zwischenablage kopieren (Strg+C)</value>
     <comment>The tooltip text that appears while hovering the copy button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonCrop.Label" xml:space="preserve">
@@ -138,7 +138,7 @@
     <comment>The text inside the button that allows the user to crop the result.</comment>
   </data>
   <data name="AppBarButtonCrop.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Zuschneiden</value>
+    <value>Scan zuschneiden</value>
     <comment>The tooltip text that appears while hovering the crop button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonDelete.Label" xml:space="preserve">
@@ -146,7 +146,7 @@
     <comment>The text inside the button that allows the user to delete the result.</comment>
   </data>
   <data name="AppBarButtonDelete.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Löschen</value>
+    <value>Scan löschen</value>
     <comment>The tooltip text that appears while hovering the delete button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonDiscard.Label" xml:space="preserve">
@@ -154,7 +154,7 @@
     <comment>The text inside the button that allows the user to discard changes.</comment>
   </data>
   <data name="AppBarButtonDiscard.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Verwerfen</value>
+    <value>Änderungen verwerfen</value>
     <comment>The tooltip text that appears while hovering the discard button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonDone.Label" xml:space="preserve">
@@ -162,7 +162,7 @@
     <comment>The text inside the button that allows the user to confirm the crop or dismiss the result.</comment>
   </data>
   <data name="AppBarButtonDone.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Fertig</value>
+    <value>Bearbeiten beenden</value>
     <comment>The tooltip text that appears while hovering the done button.</comment>
   </data>
   <data name="AppBarButtonDraw.Label" xml:space="preserve">
@@ -170,7 +170,7 @@
     <comment>The text inside the button that allows the user to draw on the result.</comment>
   </data>
   <data name="AppBarButtonDraw.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Zeichnen</value>
+    <value>Zeichnen und markieren mit Windows Ink</value>
     <comment>The tooltip text that appears while hovering the draw button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonOpenWith.Label" xml:space="preserve">
@@ -178,7 +178,7 @@
     <comment>The text inside the button that allows the user to pick an app to open the file with.</comment>
   </data>
   <data name="AppBarButtonOpenWith.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Öffnen mit</value>
+    <value>Scan in anderer App öffnen</value>
     <comment>The tooltip text that appears while hovering the open with button.</comment>
   </data>
   <data name="AppBarButtonRename.Label" xml:space="preserve">
@@ -186,7 +186,7 @@
     <comment>The text inside the button that allows the user to rename the result.</comment>
   </data>
   <data name="AppBarButtonRename.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Umbenennen</value>
+    <value>Scan umbenennen</value>
     <comment>The tooltip text that appears while hovering the rename button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonRotate.Label" xml:space="preserve">
@@ -194,7 +194,7 @@
     <comment>The text inside the button that allows the user to rotate the result.</comment>
   </data>
   <data name="AppBarButtonRotate.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Drehen</value>
+    <value>Scan 90° im Uhrzeigersinn drehen</value>
     <comment>The tooltip text that appears while hovering the rotate button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonSave.Label" xml:space="preserve">
@@ -202,7 +202,7 @@
     <comment>The text inside the button that allows the user to save the edited result.</comment>
   </data>
   <data name="AppBarButtonSave.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Speichern</value>
+    <value>Änderungen in originaler Datei speichern</value>
     <comment>The tooltip text that appears while hovering the save button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonSaveCopy.Label" xml:space="preserve">
@@ -210,7 +210,7 @@
     <comment>The text inside the button that allows the user to save the edited result as a copy.</comment>
   </data>
   <data name="AppBarButtonSaveCopy.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Kopie speichern</value>
+    <value>Änderungen in neuer Datei speichern</value>
     <comment>The tooltip text that appears while hovering the save copy button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonShare.Label" xml:space="preserve">
@@ -218,7 +218,7 @@
     <comment>The text inside the button that allows the user to share the result.</comment>
   </data>
   <data name="AppBarButtonShare.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Teilen (Strg+S)</value>
+    <value>Scan teilen (Strg+S)</value>
     <comment>The tooltip text that appears while hovering the share button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonTouchDraw.ToolTipService.ToolTip" xml:space="preserve">
@@ -238,11 +238,11 @@
     <comment>The tooltip text that appears while hovering the devices button.</comment>
   </data>
   <data name="ButtonRecents.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Vorherige Scans</value>
+    <value>Scanordner im Datei-Explorer anzeigen</value>
     <comment>The tooltip text that appears while hovering the recents button.</comment>
   </data>
   <data name="ButtonSettings.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Einstellungen</value>
+    <value>App-Einstellungen öffnen</value>
     <comment>The tooltip text that appears while hovering the settings button.</comment>
   </data>
   <data name="ButtonSettingsCancel.Content" xml:space="preserve">

Scanner/Strings/en/Resources.resw
@@ -122,7 +122,7 @@
     <comment>The text inside the button that allows the user to set an aspect ratio while cropping the result.</comment>
   </data>
   <data name="AppBarButtonAspectRatio.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Aspect Ratio</value>
+    <value>Set the aspect ratio</value>
     <comment>The tooltip text that appears while hovering the aspect ratio button.</comment>
   </data>
   <data name="AppBarButtonCopy.Label" xml:space="preserve">
@@ -130,7 +130,7 @@
     <comment>The text inside the button that allows the user to copy the result.</comment>
   </data>
   <data name="AppBarButtonCopy.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Copy (Ctrl+C)</value>
+    <value>Copy scan to clipboard (Ctrl+C)</value>
     <comment>The tooltip text that appears while hovering the copy button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonCrop.Label" xml:space="preserve">
@@ -138,7 +138,7 @@
     <comment>The text inside the button that allows the user to crop the result.</comment>
   </data>
   <data name="AppBarButtonCrop.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Crop</value>
+    <value>Crop scan</value>
     <comment>The tooltip text that appears while hovering the crop button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonDelete.Label" xml:space="preserve">
@@ -146,7 +146,7 @@
     <comment>The text inside the button that allows the user to delete the result.</comment>
   </data>
   <data name="AppBarButtonDelete.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Delete</value>
+    <value>Move scan to the Recycle Bin</value>
     <comment>The tooltip text that appears while hovering the delete button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonDiscard.Label" xml:space="preserve">
@@ -154,7 +154,7 @@
     <comment>The text inside the button that allows the user to discard changes.</comment>
   </data>
   <data name="AppBarButtonDiscard.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Discard</value>
+    <value>Discard changes</value>
     <comment>The tooltip text that appears while hovering the discard button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonDone.Label" xml:space="preserve">
@@ -162,7 +162,7 @@
     <comment>The text inside the button that allows the user to confirm the crop or dismiss the result.</comment>
   </data>
   <data name="AppBarButtonDone.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Done</value>
+    <value>Finish editing</value>
     <comment>The tooltip text that appears while hovering the done button.</comment>
   </data>
   <data name="AppBarButtonDraw.Label" xml:space="preserve">
@@ -170,7 +170,7 @@
     <comment>The text inside the button that allows the user to draw on the result.</comment>
   </data>
   <data name="AppBarButtonDraw.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Draw</value>
+    <value>Draw and highlight using Windows Ink</value>
     <comment>The tooltip text that appears while hovering the draw button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonOpenWith.Label" xml:space="preserve">
@@ -178,7 +178,7 @@
     <comment>The text inside the button that allows the user to pick an app to open the file with.</comment>
   </data>
   <data name="AppBarButtonOpenWith.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Open with</value>
+    <value>Open scan in another app</value>
     <comment>The tooltip text that appears while hovering the open with button.</comment>
   </data>
   <data name="AppBarButtonRename.Label" xml:space="preserve">
@@ -186,7 +186,7 @@
     <comment>The text inside the button that allows the user to rename the result.</comment>
   </data>
   <data name="AppBarButtonRename.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Rename</value>
+    <value>Rename scan</value>
     <comment>The tooltip text that appears while hovering the rename button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonRotate.Label" xml:space="preserve">
@@ -194,7 +194,7 @@
     <comment>The text inside the button that allows the user to rotate the result.</comment>
   </data>
   <data name="AppBarButtonRotate.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Rotate</value>
+    <value>Rotate scan 90° clockwise</value>
     <comment>The tooltip text that appears while hovering the rotate button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonSave.Label" xml:space="preserve">
@@ -202,7 +202,7 @@
     <comment>The text inside the button that allows the user to save the edited result.</comment>
   </data>
   <data name="AppBarButtonSave.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Save</value>
+    <value>Save changes to original file</value>
     <comment>The tooltip text that appears while hovering the save button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonSaveCopy.Label" xml:space="preserve">
@@ -210,7 +210,7 @@
     <comment>The text inside the button that allows the user to save the edited result as a copy.</comment>
   </data>
   <data name="AppBarButtonSaveCopy.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Save copy</value>
+    <value>Save changes to new file</value>
     <comment>The tooltip text that appears while hovering the save copy button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonShare.Label" xml:space="preserve">
@@ -218,7 +218,7 @@
     <comment>The text inside the button that allows the user to share the result.</comment>
   </data>
   <data name="AppBarButtonShare.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Share (Ctrl+S)</value>
+    <value>Share scan (Ctrl+S)</value>
     <comment>The tooltip text that appears while hovering the share button in the CommandBar.</comment>
   </data>
   <data name="AppBarButtonTouchDraw.ToolTipService.ToolTip" xml:space="preserve">
@@ -226,7 +226,7 @@
     <comment>The tooltip text that appears while hovering the button that allows for drawing with a finger in the CommandBar.</comment>
   </data>
   <data name="ButtonCancel.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Cancel</value>
+    <value>Cancel scan</value>
     <comment>The tooltip text that appears while hovering the cancel button.</comment>
   </data>
   <data name="ButtonCommandBarPrimaryFileName.ToolTipService.ToolTip" xml:space="preserve">
@@ -238,11 +238,11 @@
     <comment>The tooltip text that appears while hovering the devices button.</comment>
   </data>
   <data name="ButtonRecents.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Previous scans</value>
+    <value>Show scan folder in File Explorer</value>
     <comment>The tooltip text that appears while hovering the recents button.</comment>
   </data>
   <data name="ButtonSettings.ToolTipService.ToolTip" xml:space="preserve">
-    <value>Settings</value>
+    <value>Open app settings</value>
     <comment>The tooltip text that appears while hovering the settings button.</comment>
   </data>
   <data name="ButtonSettingsCancel.Content" xml:space="preserve">

Scanner/Utilities.cs
@@ -943,8 +943,11 @@ static class Utilities
 
     public async static void ShowRatingDialog()
     {
-        StoreContext storeContext = StoreContext.GetDefault();
-
-        await storeContext.RequestRateAndReviewAppAsync();
+        try
+        {
+            StoreContext storeContext = StoreContext.GetDefault();
+            await storeContext.RequestRateAndReviewAppAsync();
+        }
+        catch (Exception) { }
     }
 }

commit e51fbadfc3be57f5f3249c6c15515936334c6613

Bug fix
Fixed a bug introduced in a4f26129bf25b863cecbed16a7a06660e97a5057.

Scanner/MainPage.xaml.cs
@@ -99,14 +99,14 @@ namespace Scanner
             {
                 await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
                 {
-                     ComboBoxScanners.SelectedIndex = -1;
+                    ComboBoxScanners.SelectedIndex = -1;
+                    selectedScanner = null;
+                    deviceInformation.Clear();
+                    refreshLeftPanel();
+                    scannerWatcher.Start();
+                    ComboBoxScanners.IsEnabled = true;
+                    scannerWatcher.Stopped -= eventHandlerScannerWatcherStopped;
                 });
-                selectedScanner = null;
-                deviceInformation.Clear();
-                refreshLeftPanel();
-                scannerWatcher.Start();
-                ComboBoxScanners.IsEnabled = true;
-                scannerWatcher.Stopped -= eventHandlerScannerWatcherStopped;
             };
             scannerWatcher.Stopped += eventHandlerScannerWatcherStopped;
             scannerWatcher.Start();