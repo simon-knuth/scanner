d8bfa2a0e7d8313a3017989bc595da012335eb38
Simon <50021001+simon-knuth@users.noreply.github.com>
2022-05-20 13:14:36 +0200

    Advanced day & month options for custom file naming

--- Scanner/Models/FileNaming/DayFileNamingBlock.cs
+++ Scanner/Models/FileNaming/DayFileNamingBlock.cs
@@ -30,6 +30,20 @@ namespace Scanner.Models.FileNaming
             set => SetProperty(ref _Type, value);
         }
 
+        private bool _UseMinimumDigits = true;
+        public bool UseMinimumDigits
+        {
+            get => _UseMinimumDigits;
+            set => SetProperty(ref _UseMinimumDigits, value);
+        }
+
+        private int _MinimumDigits = 2;
+        public int MinimumDigits
+        {
+            get => _MinimumDigits;
+            set => SetProperty(ref _MinimumDigits, value);
+        }
+
         public bool IsValid
         {
             get => true;
@@ -48,6 +62,8 @@ namespace Scanner.Models.FileNaming
         {
             string[] parts = serialized.TrimStart('*').Split('|', StringSplitOptions.RemoveEmptyEntries);
             Type = (DayType)int.Parse(parts[1]);
+            UseMinimumDigits = bool.Parse(parts[2]);
+            MinimumDigits = int.Parse(parts[3]);
         }
 
 
@@ -56,23 +72,34 @@ namespace Scanner.Models.FileNaming
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public string ToString(ScanOptions scanOptions, DiscoveredScanner scanner)
         {
+            string result;
             DateTime currentTime = DateTime.Now;
 
             switch (Type)
             {
                 case DayType.DayOfWeek:
-                    return CultureInfo.CurrentCulture.Calendar.GetDayOfWeek(currentTime).ToString();
+                    result = CultureInfo.CurrentCulture.Calendar.GetDayOfWeek(currentTime).ToString();
+                    break;
                 case DayType.DayOfYear:
-                    return CultureInfo.CurrentCulture.Calendar.GetDayOfYear(currentTime).ToString();
+                    result = CultureInfo.CurrentCulture.Calendar.GetDayOfYear(currentTime).ToString();
+                    break;
                 case DayType.DayOfMonth:
                 default:
-                    return CultureInfo.CurrentCulture.Calendar.GetDayOfMonth(currentTime).ToString();
+                    result = CultureInfo.CurrentCulture.Calendar.GetDayOfMonth(currentTime).ToString();
+                    break;
             }
+
+            if (UseMinimumDigits)
+            {
+                result = result.PadLeft(MinimumDigits, '0');
+            }
+
+            return result;
         }
 
         public string GetSerialized()
         {
-            return $"*{Name}|{(int)Type}";
+            return $"*{Name}|{(int)Type}|{UseMinimumDigits}|{MinimumDigits}";
         }
     }
 
--- Scanner/Models/FileNaming/MonthFileNamingBlock.cs
+++ Scanner/Models/FileNaming/MonthFileNamingBlock.cs
@@ -30,6 +30,41 @@ namespace Scanner.Models.FileNaming
             set => SetProperty(ref _Type, value);
         }
 
+        private bool _AllCaps;
+        public bool AllCaps
+        {
+            get => _AllCaps;
+            set => SetProperty(ref _AllCaps, value);
+        }
+
+        private bool _UseMinimumDigits = true;
+        public bool UseMinimumDigits
+        {
+            get => _UseMinimumDigits;
+            set => SetProperty(ref _UseMinimumDigits, value);
+        }
+
+        private int _MinimumDigits = 2;
+        public int MinimumDigits
+        {
+            get => _MinimumDigits;
+            set => SetProperty(ref _MinimumDigits, value);
+        }
+
+        private bool _LimitMaxChars = false;
+        public bool LimitMaxChars
+        {
+            get => _LimitMaxChars;
+            set => SetProperty(ref _LimitMaxChars, value);
+        }
+
+        private int _MaxChars = 3;
+        public int MaxChars
+        {
+            get => _MaxChars;
+            set => SetProperty(ref _MaxChars, value);
+        }
+
         public bool IsValid
         {
             get => true;
@@ -48,6 +83,11 @@ namespace Scanner.Models.FileNaming
         {
             string[] parts = serialized.TrimStart('*').Split('|', StringSplitOptions.RemoveEmptyEntries);
             Type = (MonthType)int.Parse(parts[1]);
+            AllCaps = bool.Parse(parts[2]);
+            UseMinimumDigits = bool.Parse(parts[3]);
+            MinimumDigits = int.Parse(parts[4]);
+            LimitMaxChars = bool.Parse(parts[5]);
+            MaxChars = int.Parse(parts[6]);
         }
 
 
@@ -56,23 +96,49 @@ namespace Scanner.Models.FileNaming
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public string ToString(ScanOptions scanOptions, DiscoveredScanner scanner)
         {
+            string result;
             DateTime currentTime = DateTime.Now;
 
             switch (Type)
             {
                 case MonthType.Number:
-                    return CultureInfo.CurrentCulture.Calendar.GetMonth(currentTime).ToString();
+                    result = CultureInfo.CurrentCulture.Calendar.GetMonth(currentTime).ToString();
+
+                    if (UseMinimumDigits)
+                    {
+                        result = result.PadLeft(MinimumDigits, '0');
+                    }
+                    break;
                 case MonthType.Name:
-                    return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(currentTime.Month);
+                    result = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(currentTime.Month);
+
+                    if (LimitMaxChars)
+                    {
+                        result = result.Substring(0, MaxChars);
+                    }
+                    break;
                 case MonthType.ShortName:
                 default:
-                    return CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(currentTime.Month);
+                    result = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(currentTime.Month);
+
+                    if (LimitMaxChars)
+                    {
+                        result = result.Substring(0, MaxChars);
+                    }
+                    break;
             }
+
+            if (AllCaps)
+            {
+                result = result.ToUpper();
+            }
+
+            return result;
         }
 
         public string GetSerialized()
         {
-            return $"*{Name}|{(int)Type}";
+            return $"*{Name}|{(int)Type}|{AllCaps}|{UseMinimumDigits}|{MinimumDigits}|{LimitMaxChars}|{MaxChars}";
         }
     }
 
