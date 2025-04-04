49882ea98b4486d131012e17c6a88072e3a075b4
Simon Knuth <simon-knuth-dev@outlook.com>
2025-04-04 21:02:03 +0200

    Implement editor orientation setting

--- Scanner/Scanner/Views/EditorView.xaml.cs
+++ Scanner/Scanner/Views/EditorView.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,7 +6,9 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Scanner.Extensions;
 using Scanner.Models;
+using Scanner.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,6 +17,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 
 
 namespace Scanner.Views
@@ -23,7 +27,7 @@ namespace Scanner.Views
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // DECLARATIONS /////////////////////////////////////////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        private VirtualizingStackPanel? flipViewPanel;
 
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,6 +36,8 @@ namespace Scanner.Views
         public EditorView()
         {
             this.InitializeComponent();
+
+            ViewModel.SettingsService.PropertyChanged += SettingsService_PropertyChanged;
         }
 
 
@@ -41,6 +47,53 @@ namespace Scanner.Views
         private void ButtonRotate_ContextRequested(UIElement sender, ContextRequestedEventArgs args)
         {
             FlyoutBase.ShowAttachedFlyout(sender as FrameworkElement);
+        }
+
+        private void SettingsService_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(ISettingsService.SettingEditorOrientation):
+                    _ = ApplyFlipViewOrientationAsync(SettingEditorOrientationToOrientation(ViewModel.SettingsService.SettingEditorOrientation));
+                    break;
+            }
+        }
+
+        private async Task ApplyFlipViewOrientationAsync(Orientation orientation)
+        {
+            await this.RunOnUIThreadAndWaitAsync(DispatcherQueuePriority.Normal, () =>
+            {
+                if (flipViewPanel != null)
+                {
+                    flipViewPanel.Orientation = orientation;
+                }
+
+                // fix scrolling in vertical mode
+                if (orientation == Orientation.Vertical)
+                {
+                    ((ScrollViewer)VisualTreeHelper.GetChild(VisualTreeHelper.GetChild(FlipViewPages, 0), 0)).HorizontalScrollMode = ScrollMode.Disabled;
+                }
+                else
+                {
+                    ((ScrollViewer)VisualTreeHelper.GetChild(VisualTreeHelper.GetChild(FlipViewPages, 0), 0)).HorizontalScrollMode = ScrollMode.Enabled;
+                }
+            });
+        }
+
+        private Orientation SettingEditorOrientationToOrientation(SettingEditorOrientation setting)
+        {
+            return setting switch
+            {
+                SettingEditorOrientation.Horizontal => Orientation.Horizontal,
+                SettingEditorOrientation.Vertical => Orientation.Vertical,
+                _ => Orientation.Horizontal,
+            };
+        }
+
+        private void VirtualizingStackPanel_Loading(FrameworkElement sender, object args)
+        {
+            flipViewPanel = sender as VirtualizingStackPanel;
+            _ = ApplyFlipViewOrientationAsync(SettingEditorOrientationToOrientation(ViewModel.SettingsService.SettingEditorOrientation));
         }
     }
 }
