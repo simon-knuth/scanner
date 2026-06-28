462cf6d3f2397fcd48427f2c68069d30b7fddde1
Simon <simon-knuth-dev@outlook.com>
2026-06-28 14:58:57 +0200

    Add placeholders to HistoryView and TemplatesView

---
+++ Scanner/Scanner/Views/Converters/IntComparisonConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Scanner.Views.Converters;
+
+public partial class IntComparisonConverter : IValueConverter
+{
+    /// <summary>
+    ///     Compares the given int to the parameter.
+    /// </summary>
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value is int intValue && int.TryParse((string?)parameter, out int baseValue) && intValue == baseValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
